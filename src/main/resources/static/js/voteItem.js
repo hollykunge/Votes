9e9eb34a8c6e13ad8f03b9847a3915f0f0b9d2a2
qiaocao <50604364+qiaocao@users.noreply.github.com>
2019-10-25 19:30:19 +0800

    修正详见描述
    1、个人投票页添加上一次投票结果列；
    2、投票项上传页局部刷新表格

--- src/main/resources/static/js/voteItem.js
+++ src/main/resources/static/js/voteItem.js
@@ -40,10 +40,14 @@ function showAddModel() {
 }
 
 //定义按钮事件
-function excelImport(voteId) {
+function excelImport(voteId, fn) {
+    var fileObj = document.getElementById("file").files[0]; // js 获取文件对象
+    if (!fileObj){
+        alert('请选择一个Excel文件')
+        return;
+    }
     $('#copyCode').attr('disabled', 'disabled');
     $('#copyCode').find('.hide').removeClass('hide')
-    var fileObj = document.getElementById("file").files[0]; // js 获取文件对象
     var url = window.location.origin + "/item/import"; // 接收上传文件的后台地址
     var form = new FormData(); // FormData 对象
     if (form) {
@@ -51,34 +55,36 @@ function excelImport(voteId) {
     }
     var xhr = new XMLHttpRequest(); // XMLHttpRequest 对象
     xhr.open("post", url, false); //post方式，url为服务器请求地址，true 该参数规定请求是否异步处理。
-    xhr.onload = uploadComplete; //请求完成
+    xhr.onload = fn; //请求完成
     xhr.onerror = uploadFailed; //请求失败
     xhr.upload.onprogress = progressFunction; //【上传进度调用方法实现】
     xhr.upload.onloadstart = function () { //上传开始执行方法
         ot = new Date().getTime(); //设置上传开始时间
 
 
+
         oloaded = 0; //设置上传开始时，以上传的文件大小为0
     };
     xhr.setRequestHeader("itemId", voteId);
     xhr.send(form); //开始上传，发送form数据
-}
-
-//上传成功响应
-function uploadComplete(evt) {
-    if (evt.target.status) {
-        setInterval(function () {
-            window.location.reload();
-        }, 1000)
-
-    } else {
-        $('body').message({
-            message: '上传失败',
-            type: 'danger'
-        })
-    }
 
 }
+
+// //上传成功响应
+// function uploadComplete(evt) {
+//     if (evt.target.status) {
+//         setInterval(function () {
+//             window.location.reload();
+//         }, 1000)
+//
+//     } else {
+//         $('body').message({
+//             message: '上传失败',
+//             type: 'danger'
+//         })
+//     }
+//
+// }
 
 //上传失败
 function uploadFailed(evt) {
