b5475252f88f4b97d31b0fdb3987d77c45ef2566
zhoyou <18525788842@163.com>
2019-10-25 17:24:30 +0800

    获取远程客户端地址当做clientIp

--- src/main/java/com/hollykunge/aop/ExtApiAopIdempotent.java
+++ src/main/java/com/hollykunge/aop/ExtApiAopIdempotent.java
@@ -129,6 +129,9 @@ public class ExtApiAopIdempotent {
 		String clientIp = request.getHeader("clientIp");
 		//如果请求头中没有ip，则为本地测试，使用默认值了
 		if(StringUtils.isEmpty(clientIp)){
+			clientIp = request.getRemoteHost();
+		}
+		if(StringUtils.isEmpty(clientIp)){
 			clientIp = VoteConstants.DEFUALT_CLIENTIP;
 		}
 		return clientIp;
