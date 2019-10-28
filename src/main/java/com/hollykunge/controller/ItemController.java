c23ef673115be436d96b6c181eea728060501b45
zhoyou <18525788842@163.com>
2019-10-28 12:12:35 +0800

    设置投票项的幂等性拿到了前端处理，但是用户投票提交保存信息的幂等性后端得加上，不然可以模拟掉接口进行刷票了

--- src/main/java/com/hollykunge/controller/ItemController.java
+++ src/main/java/com/hollykunge/controller/ItemController.java
@@ -4,8 +4,6 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.hollykunge.annotation.ExtApiIdempotent;
-import com.hollykunge.annotation.ExtApiToken;
 import com.hollykunge.config.*;
 import com.hollykunge.constants.VoteConstants;
 import com.hollykunge.exception.BaseException;
@@ -197,7 +195,7 @@ public class ItemController {
      * @param model
      * @return
      */
-    @ExtApiToken(interfaceAdress = "/item/import")
+//    @ExtApiToken(interfaceAdress = "/item/import")
     @RequestMapping(value = "/voteItemsView/{id}", method = RequestMethod.GET)
     public String voteItemsView(@PathVariable Long id,
                            Model model)throws Exception {
@@ -285,7 +283,7 @@ public class ItemController {
      * @throws IOException
      */
     @RequestMapping(value = "/item/import", method = RequestMethod.POST)
-    @ExtApiIdempotent(value = VoteConstants.EXTAPIHEAD)
+//    @ExtApiIdempotent(value = VoteConstants.EXTAPIHEAD)
     public @ResponseBody Map<String,Object> excelImport(MultipartFile file, HttpServletRequest request,Model model) throws Exception {
         Map<String,Object> result = new HashMap<>();
         Map<String,Object> headerResult = new HashMap<>();
