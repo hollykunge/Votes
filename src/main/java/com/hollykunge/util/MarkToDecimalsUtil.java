fb9aed9d086b25378d6a544b2304da40887831c8
zhoyou <18525788842@163.com>
2019-11-04 14:20:32 +0800

    否同结果时统计结果的百分比能通过下一轮

--- src/main/java/com/hollykunge/util/MarkToDecimalsUtil.java
+++ src/main/java/com/hollykunge/util/MarkToDecimalsUtil.java
@@ -1,7 +1,7 @@
 package com.hollykunge.util;
 
 import com.hollykunge.exception.BaseException;
-import org.springframework.util.StringUtils;
+import com.hollykunge.model.Item;
 
 /**
  * @author: zhhongyu
@@ -10,19 +10,30 @@ import org.springframework.util.StringUtils;
  */
 public class MarkToDecimalsUtil {
 
-    public static double transfer(String mark) {
-        if(StringUtils.isEmpty(mark)){
-            throw new BaseException("参数不能为空..");
+    public static double transfer(Item item) {
+        if(judgeParams(item)){
+            String[] split = item.getAgreePassPersent().split("/");
+            double aa = Double.parseDouble(split[0])/Integer.parseInt(split[1]);
+            return aa;
         }
-        if(!mark.contains("/")){
-            throw new BaseException("参数中没有分数线 / ...");
+        return 0;
+    }
+
+    private static boolean judgeParams(Item item){
+        if(item == null){
+            throw new BaseException("参数不能为空...");
         }
-        String[] split = mark.split("/");
-        if(split.length < 1){
-            throw new BaseException("百分数字符串不规范..");
+        if(item.getAgreePassPersent() == null){
+            throw new BaseException("百分数不能为空...");
         }
-        double aa = Double.parseDouble(split[0])/Integer.parseInt(split[1]);
-        return aa;
+        if(!item.getAgreePassPersent().contains("/")){
+            throw new BaseException("没有分数...");
+        }
+        String[] split = item.getAgreePassPersent().split("/");
+        if(split.length > 2 || split.length < 1){
+            throw new BaseException("分数不符合要求...");
+        }
+        return true;
     }
 
 }
