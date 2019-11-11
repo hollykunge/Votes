2566fe43d985b175862440038bfdde4d182e515d
LARK <hollykunge@163.com>
2019-11-11 10:40:05 +0800

    修改默认用户密码123456

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -4,7 +4,7 @@ ALTER TABLE VOTE
 -- Users
 INSERT INTO USER (user_id, password, username, active)
 VALUES
-  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'user', 1);
+  (1, '$2a$10$kYdU1bOG50gufWRhAjaSLu5/EnATu6gYzxZcDsiAcltuBBfRmTowe', 'user', 1);
 
 -- Roles
 INSERT INTO ROLE (role_id, role)
