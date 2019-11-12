b9379ab3ae42a73b80cb40bfdad4e64a5f084d90
LARK <hollykunge@163.com>
2019-11-12 17:09:10 +0800

    自动一个用户登录 演示用的

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -17,20 +17,3 @@ INSERT INTO USER_ROLE (user_id, role_id)
 VALUES (1, 1);
 INSERT INTO USER_ROLE (user_id, role_id)
 VALUES (1, 2);
-
--- Votes
-INSERT INTO VOTE (vote_id, user_id, title, body, create_date)
-VALUES (1, 1, '投票项1',
-        '"投票项1"',
-        {ts '2016-10-19 11:10:13.247'});
-
--- Items
-INSERT INTO ITEM (vote_id, user_id, body, create_date,rules,agree_rule,agree_max,agree_min,agree_pass_persent)
-VALUES (1, 1,
-        '"第一轮投票"',
-        CURRENT_TIMESTAMP(),
-        '1',
-        '32',
-        10,
-        1,
-        '2/3');
