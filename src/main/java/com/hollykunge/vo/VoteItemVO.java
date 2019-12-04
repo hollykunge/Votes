9bb5560c105cc47ee2111fe1e03c08eb8a3b4ade
zhoyou <18525788842@163.com>
2019-12-04 16:10:41 +0800

    votemtemVo 增加导入字段到32个

--- src/main/java/com/hollykunge/vo/VoteItemVO.java
+++ src/main/java/com/hollykunge/vo/VoteItemVO.java
@@ -1,6 +1,5 @@
 package com.hollykunge.vo;
 
-import javax.persistence.Column;
 
 /**
  * @author: zhhongyu
@@ -37,6 +36,32 @@ public class VoteItemVO {
     private String attr5;
 
     private String attr6;
+
+    private String attr7;
+    private String attr8;
+    private String attr9;
+    private String attr10;
+    private String attr11;
+    private String attr12;
+    private String attr13;
+    private String attr14;
+    private String attr15;
+    private String attr16;
+    private String attr17;
+    private String attr18;
+    private String attr19;
+    private String attr20;
+    private String attr21;
+    private String attr22;
+    private String attr23;
+    private String attr24;
+    private String attr25;
+    private String attr26;
+    private String attr27;
+    private String attr28;
+    private String attr29;
+    private String attr30;
+    private String attr31;
 
     private Integer currentStatisticsNum;
     private Integer currentStatisticsToalScore;
@@ -176,5 +201,205 @@ public class VoteItemVO {
 
     public void setVoteItemOrder(Integer voteItemOrder) {
         this.voteItemOrder = voteItemOrder;
+    }
+
+    public String getAttr7() {
+        return attr7;
+    }
+
+    public void setAttr7(String attr7) {
+        this.attr7 = attr7;
+    }
+
+    public String getAttr8() {
+        return attr8;
+    }
+
+    public void setAttr8(String attr8) {
+        this.attr8 = attr8;
+    }
+
+    public String getAttr9() {
+        return attr9;
+    }
+
+    public void setAttr9(String attr9) {
+        this.attr9 = attr9;
+    }
+
+    public String getAttr10() {
+        return attr10;
+    }
+
+    public void setAttr10(String attr10) {
+        this.attr10 = attr10;
+    }
+
+    public String getAttr11() {
+        return attr11;
+    }
+
+    public void setAttr11(String attr11) {
+        this.attr11 = attr11;
+    }
+
+    public String getAttr12() {
+        return attr12;
+    }
+
+    public void setAttr12(String attr12) {
+        this.attr12 = attr12;
+    }
+
+    public String getAttr13() {
+        return attr13;
+    }
+
+    public void setAttr13(String attr13) {
+        this.attr13 = attr13;
+    }
+
+    public String getAttr14() {
+        return attr14;
+    }
+
+    public void setAttr14(String attr14) {
+        this.attr14 = attr14;
+    }
+
+    public String getAttr15() {
+        return attr15;
+    }
+
+    public void setAttr15(String attr15) {
+        this.attr15 = attr15;
+    }
+
+    public String getAttr16() {
+        return attr16;
+    }
+
+    public void setAttr16(String attr16) {
+        this.attr16 = attr16;
+    }
+
+    public String getAttr17() {
+        return attr17;
+    }
+
+    public void setAttr17(String attr17) {
+        this.attr17 = attr17;
+    }
+
+    public String getAttr18() {
+        return attr18;
+    }
+
+    public void setAttr18(String attr18) {
+        this.attr18 = attr18;
+    }
+
+    public String getAttr19() {
+        return attr19;
+    }
+
+    public void setAttr19(String attr19) {
+        this.attr19 = attr19;
+    }
+
+    public String getAttr20() {
+        return attr20;
+    }
+
+    public void setAttr20(String attr20) {
+        this.attr20 = attr20;
+    }
+
+    public String getAttr21() {
+        return attr21;
+    }
+
+    public void setAttr21(String attr21) {
+        this.attr21 = attr21;
+    }
+
+    public String getAttr22() {
+        return attr22;
+    }
+
+    public void setAttr22(String attr22) {
+        this.attr22 = attr22;
+    }
+
+    public String getAttr23() {
+        return attr23;
+    }
+
+    public void setAttr23(String attr23) {
+        this.attr23 = attr23;
+    }
+
+    public String getAttr24() {
+        return attr24;
+    }
+
+    public void setAttr24(String attr24) {
+        this.attr24 = attr24;
+    }
+
+    public String getAttr25() {
+        return attr25;
+    }
+
+    public void setAttr25(String attr25) {
+        this.attr25 = attr25;
+    }
+
+    public String getAttr26() {
+        return attr26;
+    }
+
+    public void setAttr26(String attr26) {
+        this.attr26 = attr26;
+    }
+
+    public String getAttr27() {
+        return attr27;
+    }
+
+    public void setAttr27(String attr27) {
+        this.attr27 = attr27;
+    }
+
+    public String getAttr28() {
+        return attr28;
+    }
+
+    public void setAttr28(String attr28) {
+        this.attr28 = attr28;
+    }
+
+    public String getAttr29() {
+        return attr29;
+    }
+
+    public void setAttr29(String attr29) {
+        this.attr29 = attr29;
+    }
+
+    public String getAttr30() {
+        return attr30;
+    }
+
+    public void setAttr30(String attr30) {
+        this.attr30 = attr30;
+    }
+
+    public String getAttr31() {
+        return attr31;
+    }
+
+    public void setAttr31(String attr31) {
+        this.attr31 = attr31;
     }
 }
