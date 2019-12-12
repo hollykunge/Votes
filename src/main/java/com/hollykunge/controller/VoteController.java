0415ceba00c6e1e0eba5309d96f6f6530fdf6c00
zhoyou <18525788842@163.com>
2019-12-12 17:01:24 +0800

    增加投票统计页面跳转接口

--- src/main/java/com/hollykunge/controller/VoteController.java
+++ src/main/java/com/hollykunge/controller/VoteController.java
@@ -130,28 +130,8 @@ public class VoteController {
             return HttpResponseUtil.setErrorResponse("没有查询到投票...");
         }
         Vote voteTemp = vote.get();
-        List<Item> itemsByVote = itemService.findItemsByVote(voteTemp);
-        Collections.sort(itemsByVote, (o1, o2) -> o2.getTurnNum().compareTo(o1.getTurnNum()));
-        //获取到了最后的一个轮次
-        Item item = itemsByVote.get(0);
-        //最后的投票结果
-        List<VoteItem> finalturnVoteItems = getfinalTurnVoteItems(item);
-        //查找是否有否同规则
-        List<Item> agareeItems = itemsByVote.stream()
-                .filter(entity -> Objects.equals(entity.getRules(), VoteConstants.ITEM_RULE_AGER))
-                .collect(Collectors.toList());
-        //倒序，最大的轮次不通过的人员，放在最上边
-        Collections.sort(agareeItems, (o1, o2) -> o2.getTurnNum().compareTo(o1.getTurnNum()));
-        List<VoteItem> result = new ArrayList<>();
-        result.addAll(finalturnVoteItems);
-        for (Item agreeItem :
-                agareeItems) {
-            result.addAll(getfinalTurnVoteItems(agreeItem)
-                    .stream()
-                    .filter(entity -> Objects.equals(entity.getAgreeRulePassFlag(), "0"))
-                    .collect(Collectors.toList()));
-        }
-        List<StatisticsDownloadData> statisticsDownloadData = JSONArray.parseArray(JSONObject.toJSONString(result), StatisticsDownloadData.class);
+        List<VoteItem> voteItems = getVoteItems(voteTemp);
+        List<StatisticsDownloadData> statisticsDownloadData = JSONArray.parseArray(JSONObject.toJSONString(voteItems), StatisticsDownloadData.class);
         LinkedHashMap jsonObject = JSON.parseObject(voteTemp.getExcelHeader(), LinkedHashMap.class);
         //清洗一遍数据，excelhead中对应的值为null的给默认值无
         AtomicInteger setId = new AtomicInteger();
@@ -189,6 +169,53 @@ public class VoteController {
         //生成文件名称
         exportExcel(resp, voteTemp.getTitle(), jsonObject, statisticsDownloadData);
         return HttpResponseUtil.setSuccessResponse();
+    }
+
+    /**
+     * 跳转到投票整体统计页面
+     * @param id
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/voteStatistics/view/{id}", method = RequestMethod.GET)
+    public String voteStatisticsView(@PathVariable Long id,
+                                     Model model) throws Exception {
+        Optional<Vote> vote = voteService.findForId(id);
+        if(vote.isPresent()){
+            Vote voteTemp = vote.get();
+            List<VoteItem> voteItems = getVoteItems(voteTemp);
+            model.addAttribute("voteItems",JSONObject.toJSONString(voteItems));
+            model.addAttribute("vote",voteTemp);
+            return "/votestatistics";
+        }else{
+            return "/error";
+        }
+    }
+
+    private List<VoteItem> getVoteItems(Vote voteTemp) throws Exception {
+        List<Item> itemsByVote = itemService.findItemsByVote(voteTemp);
+        Collections.sort(itemsByVote, (o1, o2) -> o2.getTurnNum().compareTo(o1.getTurnNum()));
+        //获取到了最后的一个轮次
+        Item item = itemsByVote.get(0);
+        //最后的投票结果
+        List<VoteItem> finalturnVoteItems = getfinalTurnVoteItems(item);
+        //查找是否有否同规则
+        List<Item> agareeItems = itemsByVote.stream()
+                .filter(entity -> Objects.equals(entity.getRules(), VoteConstants.ITEM_RULE_AGER))
+                .collect(Collectors.toList());
+        //倒序，最大的轮次不通过的人员，放在最上边
+        Collections.sort(agareeItems, (o1, o2) -> o2.getTurnNum().compareTo(o1.getTurnNum()));
+        List<VoteItem> result = new ArrayList<>();
+        result.addAll(finalturnVoteItems);
+        for (Item agreeItem :
+                agareeItems) {
+            result.addAll(getfinalTurnVoteItems(agreeItem)
+                    .stream()
+                    .filter(entity -> Objects.equals(entity.getAgreeRulePassFlag(), "0"))
+                    .collect(Collectors.toList()));
+        }
+        return result;
     }
 
     private void exportExcel(HttpServletResponse resp, String fileName, LinkedHashMap cellHeader, List data) throws IOException {
