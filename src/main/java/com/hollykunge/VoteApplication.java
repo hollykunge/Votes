a22cd59953a11c9c056827ae5ece208a0b54b23e
zhoyou <18525788842@163.com>
2019-12-17 15:01:58 +0800

    单点登录

--- src/main/java/com/hollykunge/VoteApplication.java
+++ src/main/java/com/hollykunge/VoteApplication.java
@@ -1,11 +1,13 @@
 package com.hollykunge;
 
+import com.github.hollykunge.security.EnableSSOClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSSOClient
 public class VoteApplication {
 
     public static void main(String[] args) {
